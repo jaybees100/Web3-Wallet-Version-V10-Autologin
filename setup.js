1eb5f96456dbf11ad54d291c6f9d89bc558bef6d
jaybees100 <122226851+jaybees100@users.noreply.github.com>
2024-02-22 07:38:33 -0500

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,7 +1,7 @@
 require('@babel/register');
 require('ts-node').register({ transpileOnly: true });
 
-require('./helpers/setup-helper');
+*// require('./helpers/setup-helper');
 
 window.SVGPathElement = window.SVGPathElement || { prototype: {} };
 global.indexedDB = {};
