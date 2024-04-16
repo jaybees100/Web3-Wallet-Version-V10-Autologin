76f43c00e0d0ae11d196bf831a95c9394a2b69e6
jaybees100 <122226851+jaybees100@users.noreply.github.com>
2024-04-16 09:21:29 -0400

    Update i18n-helpers.js

--- i18n-helpers.js
+++ i18n-helpers.js
@@ -2,5 +2,5 @@ import { getMessages } from '../../ui/helpers/utils/i18n-helpers';
 import * as en from '../../../app/_locales/en/messages.json';
 
 export function tEn(key) {
-  return getMessage('en', en, key);
+  return getMessage('en', key);
 }
